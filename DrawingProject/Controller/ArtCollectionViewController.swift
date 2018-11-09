2e102a9b7d9ade63c234c47bd70d67bd4c5b8a1e
Popcornblaster <lucinablaster@gmail.com>
2018-11-09 13:41:21 -0700

    Added easier navigation and more visually pleasing to look at code.

--- DrawingProject/Controller/ArtCollectionViewController.swift
+++ DrawingProject/Controller/ArtCollectionViewController.swift
@@ -46,53 +46,34 @@ class ArtCollectionViewController: UICollectionViewController
         ]
     }()
     
+    //MARK: - Lifecycle
     
     public override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
 
 
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+ public override func numberOfSections(in collectionView: UICollectionView) -> Int
+ {
+ return 1
+ }
+ 
+ 
+ public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+ {
+ return creativeCS.count
+ }
+    
     // MARK: UICollectionViewDataSource
 
-    public override func numberOfSections(in collectionView: UICollectionView) -> Int
-    {
-        return 1
-    }
-
-
-    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
-        return creativeCS.count
-    }
-//
-  //  public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
-    //{
-      //  let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        //return cell
-   // }
     
     public override func collectionView(_ collectionView: UICollectionView,
                                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
